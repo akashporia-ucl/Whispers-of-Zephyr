e7a4fec953a215593e5d750da082ae048d6b2c13
akashporia-ucl <ucabpor@ucl.ac.uk>
2024-10-22 20:54:27 +0100

    Initial commit

---
+++ blog_service/src/test/java/com/whispers_of_zephyr/blog_service/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whispers_of_zephyr.blog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
