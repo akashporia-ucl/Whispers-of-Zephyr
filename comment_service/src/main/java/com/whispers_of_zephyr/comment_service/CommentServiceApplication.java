f9b94a5b7efa5f0de7264252ee0150ea6f2695b4
akashporia-ucl <ucabpor@ucl.ac.uk>
2024-10-26 14:43:14 +0100

    Comment service setup and CI pipeline for github

---
+++ comment_service/src/main/java/com/whispers_of_zephyr/comment_service/CommentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.whispers_of_zephyr.comment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentServiceApplication.class, args);
+	}
+
+}
