8257efa2252127f31f70ff15cda62b14e9a9fd54
akashporia-ucl <ucabpor@ucl.ac.uk>
2024-11-06 18:36:34 +0000

    Basic setup of notification servic

--- comment_service/src/main/java/com/whispers_of_zephyr/comment_service/service/CommentService.java
+++ comment_service/src/main/java/com/whispers_of_zephyr/comment_service/service/CommentService.java
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Service;
 import com.whispers_of_zephyr.comment_service.model.Comment;
 import com.whispers_of_zephyr.comment_service.repository.CommentRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 @AllArgsConstructor
+@Transactional
 public class CommentService {
 
     @Autowired
